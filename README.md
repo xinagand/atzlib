c1c9866f3db6edee1a1a5af22043289f2038e986
jhlee <leejinha829@gmail.com>
2023-07-25 09:13:06 +0900

    add more info to setup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # atzlib
 bunch of python tools.
+
+Build order
+1. $ python setup.py sdist bdist_wheel
+2. $ python -m twine upload dist/*
--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
 setup(
     name='atzlib',
     version='0.0.1',
     description='Bunch of small python tools.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    
     author='jhlee29',
     author_email='leejinha829@gmail.com',
     url='https://github.com/xinagand/atzlib.git',
+    keywords=['jhlee29', 'python'],
+    
     install_requires=[],
     packages=find_packages(exclude=[]),
-    keywords=['jhlee29', 'python'],
-    python_requires='>=3.6',
     package_data={},
     zip_safe=False,
     classifiers=[
@@ -19,4 +25,5 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
+    python_requires='>=3.6',
 )
